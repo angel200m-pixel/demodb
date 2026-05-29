a45dd432ed56220329b3574b04e0820ddc5f860f
angel200m-pixel <angel200m@hotmail.com>
2026-05-28 22:57:40 -0600

    Update ProductController.java

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,22 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Product;
-import com.example.demo.service.ProductService;
+import com.example.demo.model.Review;
+import com.example.demo.service.ReviewService;
 
 @RestController
-@RequestMapping("products")
-public class ProductController {
+@RequestMapping("reviews")
+public class ReviewController {
 
     @Autowired
-    private ProductService service;
+    private ReviewService service;
     
-    @GetMapping()
-    public Iterable<Product> getAll() {
+    @GetMapping
+    public List<Review> getAll() {
         return service.getAll();
     }
 }
