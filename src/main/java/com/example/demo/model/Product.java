2ee34fffe1ed5fd4727215beb4718c577b32b0b9
angel200m-pixel <angel200m@hotmail.com>
2026-05-28 22:58:24 -0600

    Update Product.java

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -2,35 +2,41 @@ package com.example.demo.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
-@Document(collection = "products")
-public class Product {
-    
+@Document(collection = "opiniones")
+public class Review {
+
     @Id
-    @Field(targetType = FieldType.OBJECT_ID)
     private String id;
     
-    private String name;
-    private double price;
+    private String cliente;
+    private String plataforma_origen;
+    private String comentario;
+    private int calificacion_estrellas;
+    private String fecha_publicacion;
+    private String categoria_reparacion;
 
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
+    public Review() {}
+
+   
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    
+    public String getCliente() { return cliente; }
+    public void setCliente(String cliente) { this.cliente = cliente; }
+    
+    public String getPlataforma_origen() { return plataforma_origen; }
+    public void setPlataforma_origen(String plataforma_origen) { this.plataforma_origen = plataforma_origen; }
+    
+    public String getComentario() { return comentario; }
+    public void setComentario(String comentario) { this.comentario = comentario; }
+    
+    public int getCalificacion_estrellas() { return calificacion_estrellas; }
+    public void setCalificacion_estrellas(int calificacion_estrellas) { this.calificacion_estrellas = calificacion_estrellas; }
+    
+    public String getFecha_publicacion() { return fecha_publicacion; }
+    public void setFecha_publicacion(String fecha_publicacion) { this.fecha_publicacion = fecha_publicacion; }
+    
+    public String getCategoria_reparacion() { return categoria_reparacion; }
+    public void setCategoria_reparacion(String categoria_reparacion) { this.categoria_reparacion = categoria_reparacion; }
 }
